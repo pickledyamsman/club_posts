36b6e2fe41e1ccc414036161aac3fe692d920172
pickledyamsman <pickledyamsman@gmail.com>
2016-05-17 13:40:29 -0400

    edit and delete work without refresh

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,39 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  respond_to :html, :js
 
   def index
     @posts = Post.all
-  end
-
-  def create
-    @post = Post.create(post_params)
-    if @post.save
-      render json: @post, status: 201, notice: "Your post was successful!"
-    else
-      redirect_to :back, alert: "Couldn't make your post."
-    end
+    respond_with(@posts)
   end
 
   def new
     @post = Post.new
-    @post.user_id = current_user.id
+    respond_with(@post)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    flash[:notice] = 'Post was successfully created.' if @post.save
+    respond_with(@post)
   end
 
   def edit
   end
 
   def update
-    if @post.update(post_params)
-      redirect_to club_path(@post.club), notice: "Your post successfully updated!"
-    else
-      render :edit
-    end
+    flash[:notice] = 'Club successfully updated.' if @post.update(post_params)
+    respond_with(@post)
   end
 
   def show
     @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show}
-      format.json { render json: @post }
-    end
   end
 
   def destroy
     @post.destroy
-    redirect_to club_path(@post.club), notice: "Your post successfuly deleted!"
-  end
-
-  def post_data
-    post = Post.find(params[:id])
-    render json: post.to_json(only: [:title, :content, :id])
+    respond_with(@post)
   end
 
   private
